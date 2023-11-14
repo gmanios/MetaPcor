80b86f415df2ac7521c5fefd1fb4364df9ede770
Georgios A. Manios <39456816+gmanios@users.noreply.github.com>
2023-11-14 15:58:29 +0200

    Update test-myfunctions.R

--- tests/testthat/test-myfunctions.R
+++ tests/testthat/test-myfunctions.R
@@ -1,3 +1,5 @@
+library(MetaPcor)
+
 pcor <- meta_pcor(GEO_names=c("GSE76427") ,target_namespace = c('ILLUMINA_HUMANREF_8_V3'), option=2, 
           method="sparse", meta_method= "random", 
           pvalue_thres = 0.01,l1  = 0.7 ,l2 = 0)
