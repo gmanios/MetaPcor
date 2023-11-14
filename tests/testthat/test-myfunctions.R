079a6df9e2834de30c7d20192c24cc44a093787f
Georgios A. Manios <39456816+gmanios@users.noreply.github.com>
2023-11-14 19:59:20 +0200

    Update test-myfunctions.R

--- tests/testthat/test-myfunctions.R
+++ tests/testthat/test-myfunctions.R
@@ -1,12 +1,28 @@
 library(MetaPcor)
 
-pcor <- meta_pcor(GEO_names=c("GSE76427") ,target_namespace = c('ILLUMINA_HUMANREF_8_V3'), option=2, 
-          method="sparse", meta_method= "random", 
+
+
+# Test Case 1: Check if the function throws an error when an invalid GEO_names is provided.
+test_that("Invalid GEO_names should throw an error", {
+  expect_error(meta_pcor(GEO_names = c("invalid_name"), option=2, target_namespace = c('ILLUMINA_HUMANREF_8_V3')))
+})
+
+
+# Test Case 2: Check if the function throws an error when an invalid target_namespace is provided.
+test_that("Invalid target_namespace should throw an error", {
+  expect_error(meta_pcor(GEO_names=c("GSE76427"), option=2, target_namespace = c('blablablablablalbala'),method="sparse", meta_method= "random",pvalue_thres = 0.01,l1  = 0.7 ,l2 = 0))
+})
+
+
+# Test Case 3: Check if the function gives every time the same result
+
+pcor <- meta_pcor(GEO_names=c("GSE76427") ,target_namespace = c('ILLUMINA_HUMANREF_8_V3'), option=2,
+          method="sparse", meta_method= "random",
           pvalue_thres = 0.01,l1  = 0.7 ,l2 = 0)
 
 test_that("MetaPcor function", {
-  expect_equal(meta_pcor(GEO_names=c("GSE76427") ,target_namespace = c('ILLUMINA_HUMANREF_8_V3'), option=2, 
-            method="sparse", meta_method= "random", 
+  expect_equal(meta_pcor(GEO_names=c("GSE76427") ,target_namespace = c('ILLUMINA_HUMANREF_8_V3'), option=2,
+            method="sparse", meta_method= "random",
             pvalue_thres = 0.01,l1  = 0.7 ,l2 = 0),pcor)
 })
 
